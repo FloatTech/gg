02a972e0266f6ac456a7ecb834dbdbedcebbea61
源文雨 <41315874+fumiama@users.noreply.github.com>
2026-03-30 22:33:54 +0800

    refactor: move cpp into build

--- bezier_sycl.go
+++ bezier_sycl.go
@@ -6,13 +6,13 @@ import (
 	"github.com/fumiama/gozel/ze"
 )
 
-//go:generate clang++ -fsycl -fsycl-device-only -fno-sycl-instrument-device-code -fsycl-targets=spirv64 -Xclang -emit-llvm-bc bezier_sycl.cpp -o device_bezier_kern.bc
-//go:generate sycl-post-link -symbols -split=auto -emit-param-info -properties -o device_bezier_kern.table device_bezier_kern.bc
-//go:generate llvm-spirv --sycl-opt -o bezier_sycl.spv device_bezier_kern_0.bc
-//go:generate clang++ -target spirv64-unknown-unknown -S -emit-llvm -x ir device_bezier_kern_0.bc -o main.ll
-//go:generate llvm-spirv -to-text bezier_sycl.spv -o bezier_sycl.spt
+//go:generate clang++ -fsycl -fsycl-device-only -fno-sycl-instrument-device-code -fsycl-targets=spirv64 -Xclang -emit-llvm-bc build/bezier_sycl.cpp -o build/device_bezier_kern.bc
+//go:generate sycl-post-link -symbols -split=auto -emit-param-info -properties -o build/device_bezier_kern.table build/device_bezier_kern.bc
+//go:generate llvm-spirv --sycl-opt -o build/bezier_sycl.spv build/device_bezier_kern_0.bc
+//go:generate clang++ -target spirv64-unknown-unknown -S -emit-llvm -x ir build/device_bezier_kern_0.bc -o main.ll
+//go:generate llvm-spirv -to-text build/bezier_sycl.spv -o build/bezier_sycl.spt
 
-//go:embed bezier_sycl.spv
+//go:embed build/bezier_sycl.spv
 var bezierspv []byte
 
 var (
